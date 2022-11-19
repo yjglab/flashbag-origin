ed9e23d7ee4b3b685de0324c8c687c1c4f75af05
yjglab <yjgdesign@gmail.com>
2022-11-20 03:49:02 +0900

    PostForm

--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -1,8 +1,71 @@
 export const initialState = {
-  mainPosts: [{}],
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "재경",
+      },
+      content: "첫 게시글 #첫 #게시글",
+      Images: [
+        {
+          src: "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+        },
+        {
+          src: "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+        },
+        {
+          src: "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: "nero",
+          },
+          content: "wwwww",
+        },
+        {
+          User: {
+            nickname: "nero2",
+          },
+          content: "fffff",
+        },
+        {
+          User: {
+            nickname: "nero3",
+          },
+          content: "eeeee",
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
+
+const ADD_POST = "ADD_POST";
+export const addPost = {
+  type: ADD_POST,
+};
+const dummyPost = {
+  id: 2,
+  content: "dummy post",
+  User: {
+    id: 1,
+    nickname: "재경",
+  },
+  Images: [],
+  Comments: [],
 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
     default:
       return state;
   }

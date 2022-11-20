5bafc9007ffeea790297c4d47fa8531ee9ba21f4
yjglab <yjgdesign@gmail.com>
2022-11-20 20:39:42 +0900

    post card + 댓글기능

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,4 +1,4 @@
-import { Avatar, Button, Card, Popover } from "antd";
+import { Avatar, Button, Card, Comment, List, Popover } from "antd";
 import React, { useCallback, useState } from "react";
 import {
   EllipsisOutlined,
@@ -11,6 +11,7 @@ import {
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import PostImages from "./PostImages";
+import CommentForm from "./CommentForm";
 
 const PostCard = ({ post }) => {
   const id = useSelector((state) => state.user.me?.id);
@@ -65,7 +66,25 @@ const PostCard = ({ post }) => {
           description={post.content}
         />
       </Card>
-      {commentFormOpened && <div>댓글입력폼</div>}
+      {commentFormOpened && (
+        <div>
+          <CommentForm post={post} />
+          <List
+            header={`${post.Comments.length}개의 댓글`}
+            itemLayout="horizontal"
+            dataSource={post.Comments}
+            renderItem={(item) => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  content={item.content}
+                />
+              </li>
+            )}
+          />
+        </div>
+      )}
       {/* <CommentForm />
       <Comments /> */}
     </div>
